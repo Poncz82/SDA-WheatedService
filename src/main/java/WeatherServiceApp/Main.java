09f35e2c69ecc2c9669146ce004d205779a3b3e4
Michal Paukszto <michal.paukszto@gmail.com>
2021-01-24 09:52:57 +0100

    refactor

--- src/main/java/WeatherServiceApp/Main.java
+++ src/main/java/WeatherServiceApp/Main.java
@@ -1,17 +1,18 @@
-package WeatherServiceApp;
+package WeatherServiceApp; // todo change package path com.sda.weather
 
 import java.util.Scanner;
 
-public class Main {
+public class Main { // todo change name to eg. UserInterface, FrontendApplication
     public static void main(String[] args) {
         System.out.println("Aplikacja jest uruchomiona!");
 
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.println("Witaj w elektronicznym pamiętniku, co chcesz zrobić?");
+            System.out.println("Witaj w elektronicznym pamiętniku, co chcesz zrobić?"); // todo change a message
             System.out.println("1. Dodać nową lokalizacją");
             System.out.println("2. Odczytać istniejące wpisy");
+            // todo add a new possibility (obtain a weather forecast)
             System.out.println("3. Zamknąć aplikację");
 
             int response = scanner.nextInt();
